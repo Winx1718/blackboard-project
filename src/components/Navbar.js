f07883690d7b57201d282f24e030f64e1b1fe7bf
Winx1718 <98168228+Winx1718@users.noreply.github.com>
2022-09-20 21:48:25 -0500

    created file and added components and icon

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import * as FaIcons from "react-icons/fa";
+
+function Navbar() {
+  return (
+    <div>
+      <div className="navbar">
+        <Link to="#" className="menu-bars">
+          <FaIcons.FaBars />
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
