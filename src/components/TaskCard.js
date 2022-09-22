86dd29ba24ecdd5f11663afe76f3e84d5cd1d4d9
Winx1718 <98168228+Winx1718@users.noreply.github.com>
2022-09-22 17:35:24 -0500

    added taskcard

---
+++ src/components/TaskCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function TaskCard(props) {
+  return (
+      <div>
+          <form>
+              <span><input type={text} placeholder=""/></span>
+          </form>  
+    </div>
+  )
+}
+
+export default TaskCard
